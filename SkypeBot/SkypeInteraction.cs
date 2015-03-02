e85699d467bf22bdd8597fedd1ab0528cea5aa47
Alex <AlexSirko@outlook.com>
2015-03-02 16:41:06 +0200

    improvement (added default message to answer)

--- SkypeBot/SkypeInteraction.cs
+++ SkypeBot/SkypeInteraction.cs
@@ -20,6 +20,11 @@ namespace SkypeBot
         // config
         private static string mainConference = "";
         private static bool reSender = false;
+        private const string trigger = "!"; // Say !help
+        private const string nick = "BOT";
+        //private static TimeSpan start = new TimeSpan(00, 0, 0);
+        //private static TimeSpan end = new TimeSpan(01, 0, 0);
+        //private static TimeSpan now = DateTime.Now.TimeOfDay;
 
         // first
         public static bool FirstLaunch()
@@ -73,7 +78,7 @@ namespace SkypeBot
         }
 
         // unused?
-        private static List<string> FetchRawConferenceList()
+        private static List<string> FetchChatList()
         {
             List<string> list = new List<string>();
 
@@ -81,7 +86,7 @@ namespace SkypeBot
 
             foreach (IChat chat in chats)
             {
-                if (chat.Members.Count > 2)
+                if (chat.Members.Count == 2)
                 {
                     string members = "";
                     foreach (IUser user in chat.Members)
@@ -90,8 +95,7 @@ namespace SkypeBot
                     }
                     members.Trim();
 
-                    if (members.Split(' ').Contains(headAdmin))
-                        list.Add(chat.Name);
+                    list.Add(chat.Name);
                 }
             }
 
@@ -127,12 +131,33 @@ namespace SkypeBot
                 else if (message.Sender.Handle == headAdmin)
                 {
                     if (message.Body.StartsWith("."))
-                        ProcessAdminCommand(message.Chat, message.Body);
+                        try
+                        {
+                            ProcessAdminCommand(message.Chat, message.Body);
+                        }
+                        catch (Exception uuu)
+                        {
+                            Console.WriteLine("{0} Exception caught.", uuu);
+                        }
+                        
                 }
+                else if ((InBetweenDaysInclusive(DateTime.Now, DayOfWeek.Monday, DayOfWeek.Friday)) && ((TimeBetween(DateTime.Now, new TimeSpan(00, 0, 0), new TimeSpan(01, 0, 0))) || (TimeBetween(DateTime.Now, new TimeSpan(10, 0, 0), new TimeSpan(23, 59, 59)))) && (reSender))
+                    try
+                    {
+                        SendMessageToSender(message);
+                        SendMessageToMainConfrence(message);
+                    }
+                    catch (Exception uuu)
+                    {
+                        Console.WriteLine("{0} Exception caught.", uuu);
+                    }
+
                 else if (message.Chat.Name == mainConference)
                     continue;
                 else if (reSender)
                     SendMessageToMainConfrence(message);
+                
+                
             }
         }
 
@@ -148,16 +173,57 @@ namespace SkypeBot
             }
         }
 
-        private static void SendMessageToMainConfrence(string msg)
+        private static void SendMessageToSender(IChatMessage msg)
         {
-            var chats = skype.Chats;
+                // Send processed message back to skype chat window
+            skype.SendMessage(msg.Sender.Handle, "Hello, this contact is only monitored during the 1.00 AM till 10.00 AM  [GMT +2, Kiev time], (6 PM till 3 AM EST Reston time) hours on weekdays, and is monitored 24 hours a day on weekends.  Please refer your request to the relative Siebel case or contact your Manager\\TL\\TPS in the even a more timely response is required for your request.   For more details regarding this contact please refer to the Research and Development support document.  Thank You." + "\n Current Kiev time is " + DateTime.Now.ToShortTimeString() + "\n Current day of week in Kiev is " + DateTime.Now.DayOfWeek.ToString());
+        }
+
+       
+
+        private static void SendMessageToMainConfrence(string msg)
+        {var chats = skype.Chats;
 
             foreach (IChat chat in chats)
             {
                 if (chat.Name == mainConference)
                     chat.SendMessage(msg);
             }
+            
         }
+        private static bool TimeBetween(DateTime datetime, TimeSpan start, TimeSpan end)
+        {
+            // convert datetime to a TimeSpan
+            TimeSpan now = datetime.TimeOfDay;
+            // see if start comes before end
+            if (start < end)
+                return start <= now && now <= end;
+            // start is after end, so do the inverse comparison
+            return !(end < now && now < start);
+        }
+        public static bool InBetweenDaysInclusive(DateTime date, DayOfWeek start, DayOfWeek end)
+        {
+            DayOfWeek curDay = date.DayOfWeek;
+
+            if (start <= end)
+            {
+                // Test one range: start to end
+                return (start <= curDay && curDay <= end);
+            }
+            else
+            {
+                // Test two ranges: start to 6, 0 to end
+                return (start <= curDay || curDay <= end);
+            }
+        }
+   /* unused ?     
+    *   
+    * private static string ProcessMessage(string str)
+        {
+            string result="Hello, this contact is only monitored during the 1.00 AM till 10.00 AM  [GMT +2, Kiev time], (6 PM till 3 AM EST Reston time) hours on weekdays, and is monitored 24 hours a day on weekends.  Please refer your request to the relative Siebel case or contact your Manager\\TL\\TPS in the even a more timely response is required for your request.   For more details regarding this contact please refer to the Research and Development support document.  Thank You.";
+            
+            return result;
+        } */
 
         #endregion
 
@@ -184,6 +250,22 @@ namespace SkypeBot
                         returnChat.SendMessage(confsList);
                     }
                     break;
+
+                    
+                /* case ".fetchchats": // list of private chats
+                    List<string> privatechats = FetchChatList();
+                    if (privatechats.Count == 0)
+                        returnChat.SendMessage("No private chats found");
+                    else
+                    {
+                        string chatList = "";
+                        foreach (string s in privatechats )
+                        {
+                            chatList += s + Environment.NewLine;
+                        }
+                        returnChat.SendMessage(chatList);
+                    }
+                    break; */
 
                 case ".setmain":
                     if (size == 1)
